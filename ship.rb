9d7e5e7afdde22ec6e21dbcc99bd503b38dfdceb
LylyG <lgalarza@spoonflower.com>
2016-02-08 20:41:53 -0500

    Make test 12 pass. Create variables to track direct hits and prevent the user from hitting the same spot twice.

--- ship.rb
+++ ship.rb
@@ -1,36 +1,34 @@
-require 'byebug'
 require './position.rb'
+require 'byebug'
 
 class Ship
   attr_reader :length, :x_coordinate, :y_coordinate, :ship_pegs
+
   def initialize(length)
     @length = length
+    @direct_hits = []
   end
 
-  def place (x_coordinate, y_coordinate, across=true)
+  def place(x_coordinate, y_coordinate, across = true)
     return false if @placed
     @x_coordinate = x_coordinate
     @y_coordinate = y_coordinate
     @across = across
-
     ship_pegs = []
-
     if @across
       ship_pegs << Position.new(@x_coordinate, @y_coordinate)
       ship_end = x_coordinate + @length
       starting_x_coordinate = x_coordinate
-
       until starting_x_coordinate == ship_end
-      ship_pegs << Position.new(starting_x_coordinate, @y_coordinate)
-      starting_x_coordinate +=1
+        ship_pegs << Position.new(starting_x_coordinate, @y_coordinate)
+        starting_x_coordinate += 1
       end
-
     else
       ship_pegs << Position.new(@x_coordinate, @y_coordinate)
       ship_end = y_coordinate + @length
       starting_y_coordinate = y_coordinate
       until starting_y_coordinate == ship_end
-        ship_pegs << Position.new(@x_coordinate,starting_y_coordinate)
+        ship_pegs << Position.new(@x_coordinate, starting_y_coordinate)
         starting_y_coordinate +=1
       end
     end
@@ -38,23 +36,32 @@ class Ship
     @ship_pegs = ship_pegs
   end
 
-  def covers? (x_coordinate, y_coordinate)
+  def covers?(x_coordinate, y_coordinate)
     ship_pegs.each do |position|
       return true if position.x_coordinate == x_coordinate && position.y_coordinate == y_coordinate
     end
     false
   end
 
-  def overlaps_with? (other)
+  def overlaps_with?(other)
     ship_pegs.each do |position|
       return true if other.covers?(position.x_coordinate, position.y_coordinate)
     end
     false
   end
+
+  def fire_at(x_coordinate, y_coordinate)
+    @direct_hits.each do |position|
+      past_hit = (position.x_coordinate == x_coordinate) && (position.y_coordinate == y_coordinate)
+      return false if past_hit
+    end
+    if covers?(x_coordinate, y_coordinate)
+      @direct_hits << Position.new(x_coordinate, y_coordinate)
+      return true
+    else
+      @direct_hits << Position.new(x_coordinate, y_coordinate)
+      return false
+    end
+  end
+
 end
-
-boat = Ship.new(4)
-boat.place(2,1)
-
-boat2 = Ship.new(6)
-boat2.place(2,1)
