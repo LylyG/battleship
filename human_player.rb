1d3107f1f498bf26c5e959780c0b990b92ed10a2
LylyG <lgalarza@spoonflower.com>
2016-02-10 15:23:59 -0500

    Make test 31 and 32 pass. HAL can place his ships.

--- human_player.rb
+++ human_player.rb
@@ -1,7 +1,28 @@
+require './player'
+
 class HumanPlayer < Player
-  attr_reader :name
-  def initialize (name = "Dave")
+  attr_accessor :name
+
+  def initialize(name="Dave")
     @name = name
     super()
+  end
+
+  def place_ships(array)
+    ship_number = 0
+    while(ship_number < array.length)
+      ship_length = array[ship_number]
+      puts "#{name}, where would you like to place a ship of length #{ship_length}?"
+      coordinates = get_user_input
+      puts "Across or Down?"
+      direction = get_user_input
+
+      if grid.place_ship(Ship.new(ship_length), grid.x_of(coordinates),
+                       grid.y_of(coordinates), direction=="Across")
+        ship_number += 1
+      else
+        puts "Unfortunately, that ship overlaps with one of your other ships.  Please try again."
+      end
+    end
   end
 end
